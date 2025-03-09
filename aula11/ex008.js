df8341922c00b482ffe930f4fcd86ad093398a04
Eduardo Moreira Pereira Filho <eduardo.moreira230912@gmail.com>
2025-03-09 13:47:39 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78
+console.log(`A velocidade de seu carro é ${vel}km/h`)
+
+if (vel >= 60){ //condição simples!!
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
