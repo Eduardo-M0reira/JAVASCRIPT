1c3df47f4e69f5d4c66405e761b7ecd21def6f19
Eduardo Moreira Pereira Filho <eduardo.moreira230912@gmail.com>
2025-03-14 07:03:08 -0300

    Ex016  Corrigida
    Ex016 Feita e Corrigida

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,34 +1,36 @@
-var txtin = document.getElementById("txtini")
-var txtfm = document.getElementById("txtfim")
-var txtps = document.getElementById("txtpas")
+
 
 
 function Calcular(){
-    var p1 = document.getElementById("res")
-    var inicio = Number(txtin.value)
-    var fim = Number(txtfm.value)
-    var passos = Number(txtps.value)
-    p1.innerHTML = " "
-
-    if(passos == 0){
-        window.alert(`Passo inválido! Considerando PASSO 1`)
-        passos = 1
-    }
-    
-    if (inicio < fim){
-       for (var c = inicio; c <= fim; c+=passos){
-            p1.innerHTML += `${c} &#x1F449`
-            if ( c >= fim){
-                p1.innerHTML += `&#x1F3C1`
-            }
-        } 
+    let ini = document.getElementById("txtini")
+    let fm = document.getElementById("txtfim")
+    let pas = document.getElementById("txtpas")
+    var res = document.getElementById("res")
+    if (ini.value.length == 0 || fm.value.length == 0 || pas.value.length == 0){
+        res.innerHTML = `Impossível contar!!`
+        window.alert(`[ERRO] faltam dados!`)
     } else {
-        for (var c = inicio; c >= fim; c-=passos){
-            p1.innerHTML += `${c} &#x1F449`
-            if ( c <= fim){
-                p1.innerHTML += `&#x1F3C1`
-            }
-        } 
-    }
-    
+        var inicio = Number(ini.value)
+        var fim = Number(fm.value)
+        var passos = Number(pas.value)
+        res.innerHTML = "Contando: <br>"
+
+        if(passos <= 0){
+            window.alert(`Passo inválido! Considerando PASSO 1`)
+            passos = 1
+        }
+        
+        if (inicio < fim){
+            //Contagem crescente
+            for (var c = inicio; c <= fim; c += passos){
+                    res.innerHTML += `${c} \u{1F449}`
+                } 
+        } else {
+            //contagem regressiva
+            for (var c = inicio; c >= fim; c -= passos){
+                res.innerHTML += `${c} \u{1F449}`
+            } 
+        }
+        res.innerHTML += `\u{1F3C1}`
+    } 
 }
