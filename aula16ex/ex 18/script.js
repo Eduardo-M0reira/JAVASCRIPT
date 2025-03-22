ae3a7a6a397aa47181460b48edc418c1bc415b0a
Eduardo Moreira Pereira Filho <eduardo.moreira230912@gmail.com>
2025-03-22 07:48:19 -0300

    Soma

--- aula16ex/ex 18/script.js
+++ aula16ex/ex 18/script.js
@@ -5,18 +5,20 @@ function clicar(){
     n = Number(num.value)
     let novo = lista.indexOf(n)
 
-    if (num.length == 0){
+    if (num.value.length == 0){
         window.alert("Valor inválido ou já recontrado na lista")
     } else {
-        
+        if ( n > 0 && n <= 100 ){
+            if (novo == -1){
+                adicionar(n) 
+            } else {
+                window.alert("Valor inválido ou já recontrado na lista")
+            }
+        } else {
+            window.alert("Valor inválido ou já recontrado na lista")
+        }
     }
 
-    if (novo == -1){
-       adicionar(n) 
-    }
-    else {
-        window.alert("Valor inválido ou já recontrado na lista")
-    }
 }
 
 function adicionar(n){
@@ -30,6 +32,9 @@ function adicionar(n){
 }
 
 function finalizar(){
-    let res = document.getElementById("txttab")
-    res.innerHTML = "ola"
+    let res = document.getElementById("res")
+    lista.sort()
+    res.innerHTML = `<p>Ao tudo, temos ${lista.length} números cadastrados</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${lista[lista.length - 1]}</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${lista[0]}</p>`
 }
